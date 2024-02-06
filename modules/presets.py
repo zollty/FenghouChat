b28f4b65686bb3b1374c4c743ab25484ac15bab8
zollty <zollty@163.com>
2024-02-06 22:18:33 +0800

    update

--- modules/presets.py
+++ modules/presets.py
@@ -53,7 +53,7 @@ INDEX_QUERY_TEMPRATURE = 1.0
 
 CHUANHU_TITLE = i18n("风后Chat 🚀")
 
-CHUANHU_DESCRIPTION = i18n("由Bilibili [土风后虎虎](https://space.bilibili.com/29125536)、[明昭MZhao](https://space.bilibili.com/24807452) 和 [Keldos](https://github.com/Keldos-Li) 开发<br />访问风后Chat的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本")
+CHUANHU_DESCRIPTION = "FenghouChat base on [川虎](https://github.com/zollty/FenghouChat)" #i18n("由Bilibili [土风后虎虎](https://space.bilibili.com/29125536)、[明昭MZhao](https://space.bilibili.com/24807452) 和 [Keldos](https://github.com/Keldos-Li) 开发<br />访问风后Chat的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本")
 
 
 ONLINE_MODELS = [
